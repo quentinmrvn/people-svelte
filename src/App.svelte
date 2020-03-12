38d05c01d72af496d996b01ec78ce902d434cc43
Quentin MORVAN <quentin.morvan@accenture.com>
2020-03-12 09:33:22 +0100

    Search input

--- src/App.svelte
+++ src/App.svelte
@@ -3,22 +3,42 @@
 
 	import PersonCard from './components/PersonCard/PersonCard.svelte'
 	import AppBar from './components/AppBar/AppBar.svelte'
+	import SearchInput from './components/SearchInput/SearchInput.svelte'
 
 	let persons = [];
+	let search = "";
+
+	$: filterPersons = persons.filter(person => {
+		const re = new RegExp(search, 'i');
+		return person.firstname.match(re) || person.lastname.match(re);
+	});
 
 	fetchPeople().then(response => {
 		persons = response;
-	})
+	});
+
+	const onInput = (e) => {
+		search = e.currentTarget.value;
+	};
+
 </script>
 <div  class="App">
+	<header>
+		<AppBar />
+	</header>
 	<main>
-		<header>
-			<AppBar />
-		</header>
 		<div class="card-container">
-			{#each persons as person}
+			{#each filterPersons as person}
 				<PersonCard person={person}/>
 			{/each}
+		</div>
+		<div class="control-container">
+			<SearchInput
+					id="search"
+					label="search by name"
+					value={search}
+					on:input={onInput}
+			/>
 		</div>
 	</main>
 </div>
---
+++ src/components/SearchInput/SearchInput.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let name;
+    export let type;
+    export let value;
+    export let disabled = false;
+    export let isInvalid = false;
+    export let label;
+    export let placeholder;
+
+    const idForName = name => `person-form-${name}`;
+</script>
+
+<div class="SearchInput input-field">
+    <i class="material-icons prefix"></i>
+    <input
+            id={idForName}
+            name={name}
+            type='text'
+            value={value}
+            disabled={disabled}
+            class={isInvalid ? 'invalid' : undefined}
+            on:input
+            autoComplete="off"
+    />
+    <label for={idForName(name)} class={value ? 'active' : undefined}>
+        {label}
+    </label>
+</div>
